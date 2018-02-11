7f62e842cf0aefcbc3050dd9cfe3c1e49e5f7302
Evrim P <evrimfeyyaz@gmail.com>
2018-02-11 12:05:07 +0300

    Instead of #optional and #allows_multiple_selections in room service item choices, only use #maximum_number_of_selections and #minimum_number_of_selections

--- app/models/room_service_item_choice.rb
+++ app/models/room_service_item_choice.rb
@@ -4,11 +4,17 @@ class RoomServiceItemChoice < ApplicationRecord
   has_many :room_service_item_choice_options
   belongs_to :default_option, class_name: 'RoomServiceItemChoiceOption'
 
-  attribute :optional, :boolean, default: true
-  attribute :allows_multiple_selections, :boolean, default: false
-
   validates_presence_of :title
-  validates_inclusion_of :optional, in: [true, false]
-  validates_inclusion_of :allows_multiple_selections, in: [true, false]
   validates_length_of :room_service_item_choice_options, minimum: 2
+  validates_numericality_of :minimum_number_of_selections, greater_than: 0, only_integer: true, allow_nil: true
+
+  validate :maximum_number_of_selections_is_greater_than_or_equal_to_minimum
+
+  def maximum_number_of_selections_is_greater_than_or_equal_to_minimum
+    return if maximum_number_of_selections.nil? || minimum_number_of_selections.nil?
+    return if maximum_number_of_selections >= minimum_number_of_selections
+
+    errors.add(:maximum_number_of_selections, :less_than_minimum,
+               message: "maximum number of selections can't be less than the minimum")
+  end
 end
--- spec/models/room_service_item_choice_spec.rb
+++ spec/models/room_service_item_choice_spec.rb
@@ -6,34 +6,94 @@ describe RoomServiceItemChoice do
   it { should belong_to(:default_option) }
 
   it { should validate_presence_of :title }
-  it { should_not allow_value(nil).for(:allows_multiple_selections) }
-  it { should_not allow_value(nil).for(:optional) }
+  it { should validate_numericality_of(:minimum_number_of_selections).is_greater_than(0).only_integer }
+  it { should allow_value(nil).for(:minimum_number_of_selections) }
+  it { should allow_value(nil).for(:maximum_number_of_selections) }
 
-  describe '#optional' do
-    it 'has a default value of "true"' do
-      expect(subject.optional).to eq(true)
-    end
-  end
-
-  describe '#allows_multiple_selections' do
-    it 'has a default value of "true"' do
-      expect(subject.allows_multiple_selections).to eq(false)
-    end
-  end
-
-  describe 'has at least two options validation' do
-    it 'is not valid when it has less than two options' do
+  describe 'validate length of room service item choice options is minimum two' do
+    it 'does not pass when there are less than two options' do
       subject.validate
 
       expect(subject.errors.details[:room_service_item_choice_options]).to include(error: :too_short, count: 2)
     end
 
-    it 'is valid when it has two or more options' do
+    it 'passes when there are two or more options' do
       subject.room_service_item_choice_options << build_list(:room_service_item_choice_option, 2)
 
       subject.validate
 
-      expect(subject.errors.details[:room_service_item_choice_options]).to be_empty
+      expect(subject).not_to have_validation_error_on(:room_service_item_choice_options)
+                               .with_message_symbol(:too_short)
+    end
+  end
+
+  describe 'validate maximum number of selections is greater than or equal to minimum' do
+    it 'does not pass when maximum is less than minimum' do
+      subject.minimum_number_of_selections = 3
+      subject.maximum_number_of_selections = 2
+
+      subject.validate
+
+      expect(subject).to have_validation_error_on(:maximum_number_of_selections)
+                           .with_message_symbol(:less_than_minimum)
+                           .with_message_string("maximum number of selections can't be less than the minimum")
+    end
+
+    it 'passes when maximum is equal to minimum' do
+      subject.minimum_number_of_selections = 1
+      subject.maximum_number_of_selections = 1
+
+      subject.validate
+
+      expect(subject).not_to have_validation_error_on(:maximum_number_of_selections)
+                               .with_message_symbol(:less_than_minimum)
+                               .with_message_string("maximum number of selections can't be less than the minimum")
+    end
+
+    it 'passes when maximum is greater than minimum' do
+      subject.minimum_number_of_selections = 2
+      subject.maximum_number_of_selections = 3
+
+      subject.validate
+
+      expect(subject).not_to have_validation_error_on(:maximum_number_of_selections)
+                               .with_message_symbol(:less_than_minimum)
+                               .with_message_string("maximum number of selections can't be less than the minimum")
+    end
+
+    it 'passes when maximum is a number and minimum is nil' do
+      subject.minimum_number_of_selections = nil
+      subject.maximum_number_of_selections = 2
+
+      subject.validate
+
+      expect(subject).not_to have_validation_error_on(:maximum_number_of_selections)
+                               .with_message_symbol(:less_than_minimum)
+                               .with_message_string("maximum number of selections can't be less than the minimum")
+    end
+
+    context 'when maximum is nil' do
+      it 'passes when minimum is nil' do
+        subject.minimum_number_of_selections = nil
+        subject.maximum_number_of_selections = nil
+
+        subject.validate
+
+        expect(subject).not_to have_validation_error_on(:maximum_number_of_selections)
+                                 .with_message_symbol(:less_than_minimum)
+                                 .with_message_string("maximum number of selections can't be less than the minimum")
+      end
+
+      it 'passes when minimum is a number' do
+        subject.minimum_number_of_selections = 3
+        subject.maximum_number_of_selections = nil
+
+        subject.validate
+
+        expect(subject).not_to have_validation_error_on(:maximum_number_of_selections)
+                                 .with_message_symbol(:less_than_minimum)
+                                 .with_message_string("maximum number of selections can't be less than the minimum")
+      end
     end
   end
 end
