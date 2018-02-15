65ed645ce9a529f3dad232c5b2c4431c68cd3d73
Evrim Persembe <evrimfeyyaz@users.noreply.github.com>
2018-02-15 10:38:28 +0300

    Ensure that default option is provided when a choice is a single-selection choice (#5)

--- app/models/room_service_item_choice.rb
+++ app/models/room_service_item_choice.rb
@@ -9,12 +9,23 @@ class RoomServiceItemChoice < ApplicationRecord
   validates_numericality_of :minimum_number_of_selections, greater_than: 0, only_integer: true, allow_nil: true
 
   validate :maximum_number_of_selections_is_greater_than_or_equal_to_minimum
+  validate :existence_of_default_option_if_single_selection_choice
 
-  def maximum_number_of_selections_is_greater_than_or_equal_to_minimum
-    return if maximum_number_of_selections.nil? || minimum_number_of_selections.nil?
-    return if maximum_number_of_selections >= minimum_number_of_selections
+  private
 
-    errors.add(:maximum_number_of_selections, :less_than_minimum,
-               message: "maximum number of selections can't be less than the minimum")
-  end
+    def maximum_number_of_selections_is_greater_than_or_equal_to_minimum
+      return if maximum_number_of_selections.nil? || minimum_number_of_selections.nil?
+      return if maximum_number_of_selections >= minimum_number_of_selections
+
+      errors.add(:maximum_number_of_selections, :less_than_minimum,
+                 message: "maximum number of selections can't be less than the minimum")
+    end
+
+    def existence_of_default_option_if_single_selection_choice
+      return if default_option.present?
+      return if maximum_number_of_selections.nil? || maximum_number_of_selections > 1
+
+      errors.add(:default_option, :should_exist,
+                 message: 'default option should be provided when the maximum number of selections is "1"')
+    end
 end
