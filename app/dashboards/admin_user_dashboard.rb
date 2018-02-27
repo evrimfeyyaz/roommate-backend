4e34a084c4f9f8d4a52b2170d7e95763f1e7e903
Evrim P <evrimfeyyaz@gmail.com>
2018-02-27 22:09:05 +0300

    Create admin dashboards for admin users and hotels

--- app/dashboards/admin_user_dashboard.rb
+++ app/dashboards/admin_user_dashboard.rb
@@ -1,6 +1,6 @@
 require "administrate/base_dashboard"
 
-class AdminDashboard < Administrate::BaseDashboard
+class AdminUserDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
   # a hash that describes the type of each of the model's fields.
   #
@@ -36,10 +36,10 @@ class AdminDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :id,
     :email,
-    :encrypted_password,
-    :reset_password_token,
+    :sign_in_count,
+    :current_sign_in_at,
+    :current_sign_in_ip,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
@@ -47,7 +47,6 @@ class AdminDashboard < Administrate::BaseDashboard
   SHOW_PAGE_ATTRIBUTES = [
     :id,
     :email,
-    :encrypted_password,
     :reset_password_token,
     :reset_password_sent_at,
     :remember_created_at,
@@ -71,29 +70,13 @@ class AdminDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-    :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
-    :sign_in_count,
-    :current_sign_in_at,
-    :last_sign_in_at,
-    :current_sign_in_ip,
-    :last_sign_in_ip,
-    :confirmation_token,
-    :confirmed_at,
-    :confirmation_sent_at,
-    :unconfirmed_email,
-    :failed_attempts,
-    :unlock_token,
-    :locked_at,
+    :email
   ].freeze
 
   # Overwrite this method to customize how admins are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(admin)
-  #   "Admin ##{admin.id}"
-  # end
+  def display_resource(admin_user)
+    "Admin User (#{admin_user.email})"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  root to: 'admin#index'
+  root to: 'admin/hotels#index'
 
   # Admin Users
   devise_for :admin_users
+
+  # Admin
+  namespace :admin do
+    root to: 'hotels#index'
+
+    resources :admin_users
+    resources :hotels
+  end
 
   # GraphQL API
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
